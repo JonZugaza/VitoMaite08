372683a96caf7ec88b9d5638e59af92d54891fb8
JonZugaza <jonzugaza23@gmail.com>
2024-11-10 20:30:31 +0100

    se abre la BD

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,3 +1,18 @@
-//aqui se carga la BD de la aplicacion
 
+document.addEventListener('DOMContentLoaded', function () {
+    inicializar();
+});
+
+function inicializar() {
+    var solicitud = indexedDB.open("vitomaite08", 1);
+
+    solicitud.onerror = function (evento) {
+        console.error("la BD no se ha abierto", evento.target.error);
+    };
+
+    solicitud.onsuccess = function (evento) {
+        var db = evento.target.result;
+        console.log("BD abierta", db);
+    };
+}
 
