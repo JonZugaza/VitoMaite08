2e5303742732a5109d4c9aa67dd2277e3289ebab
JonZugaza <jonzugaza23@gmail.com>
2024-11-19 16:16:58 +0100

    metidas las aficiones

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -14,5 +14,28 @@ function inicializar() {
         var db = evento.target.result;
         console.log("BD abierta", db);
     };
+
+    solicitud.onupgradeneeded = function (evento) {
+        var db = evento.target.result;
+
+        var aficionesStore = db.createObjectStore("Aficiones", {keyPath: "id", autoIncrement: true});
+        aficionesStore.add({nombre: "Fútbol"});
+        aficionesStore.add({nombre: "Escuchar música"});
+        aficionesStore.add({nombre: "Viajar"});
+        aficionesStore.add({nombre: "Programar"});
+        aficionesStore.add({nombre: "VideoJuegos"});
+        aficionesStore.add({nombre: "Salir a comer"});
+        aficionesStore.add({nombre: "Natación"});
+        aficionesStore.add({nombre: "Basket"});
+        aficionesStore.add({nombre: "Ir al gym"});
+        aficionesStore.add({nombre: "Cocinar"});
+        aficionesStore.add({nombre: "Leer"});
+        aficionesStore.add({nombre: "Ver series"});
+        aficionesStore.add({nombre: "Salir de compras"});
+        aficionesStore.add({nombre: "Tocar instrumentos"});
+        aficionesStore.add({nombre: "Animales"});
+
+        aficionesStore.createIndex("nombre", "nombre", {unique: true});
+    };
 }
 
