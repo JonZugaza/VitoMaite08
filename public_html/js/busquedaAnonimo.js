ee6bc847befb3095859f9a17c2fe75ee16a8cfe4
JonZugaza <jonzugaza23@gmail.com>
2024-11-21 19:46:34 +0100

    la lógica de la búsqueda debería funcionar, falta que salgan los usuarios

--- public_html/js/busquedaAnonimo.js
+++ public_html/js/busquedaAnonimo.js
@@ -2,6 +2,70 @@ document.getElementById("botonBuscar").addEventListener("click", function () {
 buscar();
 });
 
-function buscar(){
+function buscar() {
     
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = ""; // Limpiar resultados previos
+
+    
+    var solicitud = indexedDB.open("vitomaite08", 1);
+
+    solicitud.onsuccess = function (evento) {
+        var db = evento.target.result;
+
+        
+        if (!db.objectStoreNames.contains("Usuarios")) {
+            console.error("El objectStore 'Usuarios' no existe.");
+            console.error("No hay datos de usuarios disponibles.");
+            return;
+        }
+
+        var transaccion = db.transaction(["Usuarios"], "readonly");
+        var usuariosStore = transaccion.objectStore("Usuarios");
+
+        var cursor = usuariosStore.openCursor();
+
+        var hayUsuariosEnTabla = false;
+        var hayUsuariosQueCumplen = false;
+
+        var generoSeleccionado = document.getElementById("genero").value;
+        var edadMin = parseInt(document.getElementById("edadMin").value) || 18; 
+        var edadMax = parseInt(document.getElementById("edadMax").value) || 100; 
+        var ciudadSeleccionada = document.getElementById("ciudad").value;
+
+        cursor.onsuccess = function (eventoCursor) {
+            var resultado = eventoCursor.target.result;
+
+            if (resultado) {
+                hayUsuariosEnTabla = true;
+
+                var usuario = resultado.value;
+
+                var cumpleCriterios =
+                    (usuario.genero === generoSeleccionado && 
+                    usuario.edad >= edadMin && usuario.edad <= edadMax &&
+                    usuario.ciudad === ciudadSeleccionada
+                    ); 
+            
+                if (cumpleCriterios) {
+                    console.log("se van a devolver personas");
+                    //función donde se muestran los usuarios
+                    hayUsuariosQueCumplen = true;
+                }
+                resultado.continue();
+            } 
+            else {
+                
+                if (!hayUsuariosEnTabla) {
+                    console.error("No hay usuarios");
+                } else if (!hayUsuariosQueCumplen) {
+                    console.error("No se cumplen los criterios");
+                }
+            }
+        };
+    };
+
+    solicitud.onerror = function () {
+        console.error("no se abre la bd.");
+    };
 }
