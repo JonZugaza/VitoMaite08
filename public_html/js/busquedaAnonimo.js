61476f2d38d44c71c55bd6e702b0cfc23706e110
JonZugaza <jonzugaza23@gmail.com>
2024-11-21 19:01:23 +0100

    empezar buscar anónimo

--- public_html/js/busquedaAnonimo.js
+++ public_html/js/busquedaAnonimo.js
@@ -1,6 +1,7 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
+document.getElementById("botonBuscar").addEventListener("click", function () {
+buscar();
+});
 
-
+function buscar(){
+    
+}
