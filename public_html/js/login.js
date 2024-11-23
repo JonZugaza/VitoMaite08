7a70f4037904de5de20cddc0f30c34d1da8ab40e
JonZugaza <jonzugaza23@gmail.com>
2024-11-23 12:30:15 +0100

    inicio de sesión en el login igual al del index

--- public_html/js/login.js
+++ public_html/js/login.js
@@ -1,6 +1,55 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
+document.getElementById("botonIS").addEventListener("click", function () {
+iniciarSesion();
+});
+function iniciarSesion()
+{
+var email = document.getElementById("email").value;
+        var contraseña = document.getElementById("contraseña").value;
+        var solicitud = indexedDB.open("vitomaite08", 1);
+        solicitud.onsuccess = function (evento)
+        {
+            console.error("klk")
+        var db = evento.target.result;
+                var transaccion = db.transaction(["Usuarios"], "readonly");
+                var usuariosStore = transaccion.objectStore("Usuarios");
+                var indiceEmail = usuariosStore.index("email");
+                var cursor = indiceEmail.openCursor(IDBKeyRange.only(email));
+                cursor.onsuccess = function (eventoCursor)
+                {
+
+                var resultado = eventoCursor.target.result;
+                        if (resultado)
+                {
+
+                if (resultado.value.contraseña === contraseña)
+                {
+
+                sessionStorage.setItem('email', resultado.value.email);
+                        sessionStorage.setItem('nombre', resultado.value.nombre);
+                        sessionStorage.setItem('foto', resultado.value.foto);
+                        sessionStorage.setItem('genero', resultado.value.genero);
+                       if (resultado.value.esPremium === "0")
+                        window.location.href = 'menuLogueado.html';
+                        else
+                        window.location.href = 'menuPremium.html';
+                }
+                else
+                {
+                mostrarMensaje("Error: Credenciales incorrectas");
+                }
+                }
+                else
+                {
+                mostrarMensaje("Error: Usuario no encontrado");
+                }
+                };
+        };
+    };
+
+function mostrarMensaje(mensaje) {
+var mensajeDiv = document.getElementById("mensaje");
+        mensajeDiv.innerHTML = mensaje;
+}
+
 
 
