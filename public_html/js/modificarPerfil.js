15e35902299a8dd8c457ce67681c7bb035394777
Hodei Garmendia Fuente <hodeigarmendia@gmail.com>
2024-11-25 22:08:47 +0100

    modificar perfil casi

--- public_html/js/modificarPerfil.js
+++ public_html/js/modificarPerfil.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var foto = sessionStorage.getItem('foto');
     var mensajeBienvenida = document.getElementById("mensajeBienvenida");
     var genero = sessionStorage.getItem('genero');
-    
+
     if (genero === 'H') {
         mensajeBienvenida.textContent = "Bienvenido, " + nombre;
     } else {
@@ -31,13 +31,76 @@ ciudades.forEach(ciudad => {
 });
 
 
-
-var botonGuardar = document.getElementById('botonGuardar');
-
-
-function cambiarCiudad(){
+function cambiarCiudad() {
+    var ciudadSeleccionada = document.getElementById("ciudadSeleccionada").value;
     
-    var selectCiudad = document.getElementById("ciudadSeleccionada").value;
+    if(!ciudadSeleccionada)
+    {
+        alert("Por favor, selecciona una ciudad.");
+        return;
     }
- 
+    
+    var emailUsuario = sessionStorage.getItem("email");
+    if(!emailUsuario){
+        alert("No se encontró al usuario. Inicia sesión primero.")
+        return;
+    }
+    
+     var solicitud = indexedDB.open("vitomaite08", 1);
+
+    solicitud.onsuccess = function (evento) {
+        var db = evento.target.result;
+        var transaccion = db.transaction(["Usuarios"], "readwrite");
+        var usuariosStore = transaccion.objectStore("Usuarios");
+
+    var solicitudUsuario = usuariosStore.get(emailUsuario);
+        solicitudUsuario.onsuccess = function () {
+            var usuario = solicitudUsuario.result;
+            if (usuario) {
+                usuario.ciudad = ciudadSeleccionada;
+                usuariosStore.put(usuario); 
+                sessionStorage.setItem("ciudad", ciudadSeleccionada); 
+
+                Swal.fire({
+                    icon: "success",
+                    title: "Ciudad actualizada",
+                    text: "Tu ciudad ha sido actualizada"
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Usuario no encontrado",
+                    text: "No se encontró tu perfil en la base de datos."
+                });
+            }
+        };
+    solicitudUsuario.onerror = function () {
+            Swal.fire({
+                icon: "error",
+                title: "Error al actualizar",
+                text: "No se pudo actualizar la ciudad."
+            });
+        };
+    };
+
+    solicitud.onerror = function () {
+        Swal.fire({
+            icon: "error",
+            title: "Error con la base de datos",
+            text: "No se pudo abrir la base de datos."
+        });
+    };
+}
+    
+        guardarCiudad.addEventListener("click", cambiarCiudad);
+
+    
+    
+    
+    
+    
+    
+
+
+
 
