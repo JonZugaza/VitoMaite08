403222a7bd94961eea196eac5d23360226a9823c
Hodei Garmendia Fuente <hodeigarmendia@gmail.com>
2024-11-25 12:48:23 +0100

    modificar perfil

--- public_html/js/modificarPerfil.js
+++ public_html/js/modificarPerfil.js
@@ -19,5 +19,25 @@ document.getElementById("botonCS").addEventListener('click', function () {
     window.location.href = 'index.html';
 });
 
+var selectCiudad = document.createElement('select');
+selectCiudad.id = 'ciudadSeleccionada';
 
+var ciudades = ['bilbao', 'donosti', 'vitoria'];
+ciudades.forEach(ciudad => {
+    const option = document.createElement('option');
+    option.value = ciudad.toLowerCase();
+    option.textContent = ciudad;
+    selectCiudad.appendChild(option);
+});
+
+
+
+var botonGuardar = document.getElementById('botonGuardar');
+
+
+function cambiarCiudad(){
+    
+    var selectCiudad = document.getElementById("ciudadSeleccionada").value;
+    }
+ 
 
