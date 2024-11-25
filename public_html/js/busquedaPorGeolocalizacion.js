eaea51a2f1ad111ad51f1de5b591debfa8c15791
JonZugaza <jonzugaza23@gmail.com>
2024-11-25 18:27:08 +0100

    aparecen todos los marcadores

--- public_html/js/busquedaPorGeolocalizacion.js
+++ public_html/js/busquedaPorGeolocalizacion.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var foto = sessionStorage.getItem('foto');
     var mensajeBienvenida = document.getElementById("mensajeBienvenida");
     var genero = sessionStorage.getItem('genero');
-
     if (genero === 'H') {
         mensajeBienvenida.textContent = "Bienvenido, " + nombre;
     } else {
@@ -13,17 +12,15 @@ document.addEventListener('DOMContentLoaded', function () {
     var fotoUsuarioElement = document.getElementById("foto");
     fotoUsuarioElement.src = foto;
 
+    ponerPersonas();
 });
-
 var latUsuario = parseFloat(sessionStorage.getItem('lat'));
 var longUsuario = parseFloat(sessionStorage.getItem('long'));
 var nombre = sessionStorage.getItem("nombre");
-var map; // Variable global para guardar el mapa
+var map; 
 var circle;
-
 console.log(latUsuario);
 console.log(longUsuario);
-
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: latUsuario, lng: longUsuario},
@@ -31,40 +28,27 @@ function initMap() {
 
     });
 
-    const marker = new google.maps.Marker({
-        position: {lat: latUsuario, lng: longUsuario},
-        map
-    });
-
-    marker.addListener("click", () => {
-        alert(`Has hecho clic en la casa de ` + nombre);
-    });
 }
 
 document.getElementById("botonCS").addEventListener('click', function () {
     sessionStorage.clear();
     window.location.href = 'index.html';
 });
-
 document.getElementById("botonBuscar").addEventListener("click", function () {
     cambiarCirculo();
 });
-
 function cambiarCirculo() {
     var cercania = document.getElementById("cercania").value;
     var radio;
-
     if (cercania === "0.5") {
         console.log("0.5");
         radio = 0.5;
     } else if (cercania === "1") {
         console.log("1");
         radio = 1;
-
     } else if (cercania === "1.5") {
         console.log("1.5");
         radio = 1.5;
-
     } else if (cercania === "2") {
         console.log("2");
         radio = 2;
@@ -74,7 +58,7 @@ function cambiarCirculo() {
     } else if (cercania === "10") {
         console.log("10");
         radio = 10;
-    } else{
+    } else {
         console.log("50");
         radio = 50;
     }
@@ -82,20 +66,76 @@ function cambiarCirculo() {
 }
 
 function dibujarCirculo(radio) {
-    // Eliminar el círculo anterior si existe
     if (circle) {
-        circle.setMap(null); // Remueve el círculo del mapa
+        circle.setMap(null); 
     }
 
-    // Crear un nuevo círculo y asignarlo a la variable global
     circle = new google.maps.Circle({
         strokeColor: "#FF0000",
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: "#FF0000",
         fillOpacity: 0.2,
-        map, // Usar el mapa global creado en `initMap`
+        map, 
         center: {lat: latUsuario, lng: longUsuario},
-        radius: radio * 1000 // Convertir kilómetros a metros
+        radius: radio * 1000 
     });
 }
+
+function ponerPersonas() {
+    var solicitud = indexedDB.open("vitomaite08", 1);
+
+    solicitud.onsuccess = function (evento) {
+        var db = evento.target.result;
+
+        if (!db.objectStoreNames.contains("Usuarios")) {
+            console.error("El objectStore 'Usuarios' no existe.");
+            console.error("No hay datos de usuarios disponibles.");
+            return;
+        }
+
+        var transaccion = db.transaction(["Usuarios"], "readonly");
+        var usuariosStore = transaccion.objectStore("Usuarios");
+
+        var cursor = usuariosStore.index("edad").openCursor();
+        var hayUsuariosEnTabla = false;
+        var hayUsuariosQueCumplen = false;
+
+        cursor.onsuccess = function (eventoCursor) {
+            var resultado = eventoCursor.target.result;
+
+            if (resultado) {
+                hayUsuariosEnTabla = true; 
+                var usuario = resultado.value;
+                var usuarioLat = parseFloat(usuario.lat);
+                var usuarioLong = parseFloat(usuario.long);
+
+                const marker = new google.maps.Marker({
+                    position: {lat: usuarioLat, lng: usuarioLong},
+                    map
+                });
+
+                marker.addListener("click", () => {
+                    alert(`Has hecho clic en la casa de ` + usuario.nombre);
+                });
+                resultado.continue();
+            } else {
+                if (!hayUsuariosEnTabla) {
+                    console.error("No hay usuarios.");
+                } else if (!hayUsuariosQueCumplen) {
+                    console.error("No se cumplen los criterios.");
+                }
+            }
+        };
+
+        cursor.onerror = function () {
+            console.error("Error al recorrer el cursor.");
+        };
+    };
+
+    solicitud.onerror = function () {
+        console.error("No se pudo abrir la base de datos.");
+    };
+}
+
+
